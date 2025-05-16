8057db91c5ff8d59eab19d089115772adcb428b7
wepickplatform <sanghyeon@wepick.kr>
2025-05-16 18:07:50 +0900

    Update FeaturedSlider.tsx

--- src/components/FeaturedSlider.tsx
+++ src/components/FeaturedSlider.tsx
@@ -1,45 +1,9 @@
 'use client'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, Autoplay } from 'swiper/modules'
-import Image from 'next/image'
-import Link from 'next/link'
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-
 export default function FeaturedSlider({ posts }) {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Autoplay]}
-      spaceBetween={30}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 5000 }}
-      className="rounded-xl overflow-hidden"
-    >
-      {posts.map(post => (
-        <SwiperSlide key={post.id}>
-          <div className="relative h-[500px] w-full">
-            <Image
-              src={post.featuredImage?.sourceUrl || '/images/placeholder.jpg'}
-              alt={post.title}
-              fill
-              className="object-cover"
-            />
-            <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent flex flex-col justify-end p-8">
-              <div className="text-white">
-                <h3 className="text-3xl font-bold mb-2">{post.title}</h3>
-                <div className="mb-4" dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-                <Link href={`/post/${post.slug}`} className="btn-primary inline-block">
-                  자세히 보기
-                </Link>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  )
+    <div className="bg-gray-200 h-[500px] rounded-xl flex items-center justify-center">
+      <p className="text-gray-600">슬라이더가 여기에 표시됩니다</p>
+    </div>
+  );
 }
