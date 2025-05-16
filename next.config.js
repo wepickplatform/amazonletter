d5046255af439c9c939d8d5282bf1d79fc800daf
wepickplatform <sanghyeon@wepick.kr>
2025-05-16 18:08:11 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['letter.wepick.kr'], // 워드프레스 이미지 도메인 추가
-  },
-  env: {
-    WORDPRESS_API_URL: process.env.WORDPRESS_API_URL,
+    domains: ['letter.wepick.kr'],
+    unoptimized: true,
   },
 }
 
